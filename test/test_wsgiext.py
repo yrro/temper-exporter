14ecff2223041fc5796d2ce7887b38d081036b16
Sam Morris <sam@robots.org.uk>
2017-02-22 12:19:51 +0000

    Parametrize more tests

--- test/test_wsgiext.py
+++ test/test_wsgiext.py
@@ -65,22 +65,16 @@ def test_IPv64Server(address, v6only, expected_family, expected_v6only):
         else:
             raise
 
-def test_HealthCheckServer_200():
+@pytest.mark.parametrize('status, expected', [
+    ('200 OK', True),
+    ('500 Not OK', False),
+])
+def test_HealthCheckServer(status, expected):
     s = wsgiext.HealthCheckServer(('', 0), simple_server.WSGIRequestHandler)
-    s.set_app(functools.partial(app, '200 OK'))
+    s.set_app(functools.partial(app, status))
     t = threading.Thread(target=functools.partial(s.serve_forever), daemon=True)
     t.start()
-    assert s.healthy()
-    s.shutdown()
-    t.join()
-    s.server_close()
-
-def test_HealthCheckServer_500():
-    s = wsgiext.HealthCheckServer(('', 0), simple_server.WSGIRequestHandler)
-    s.set_app(functools.partial(app, '500 Not OK'))
-    t = threading.Thread(target=functools.partial(s.serve_forever), daemon=True)
-    t.start()
-    assert not s.healthy()
+    assert s.healthy() == expected
     s.shutdown()
     t.join()
     s.server_close()
@@ -90,31 +84,25 @@ class SRH(wsgiext.SilentRequestHandler):
     def log_date_time_string(self):
         return ':datetime:'
 
-def test_SilentRequestHandler_200(capsys):
+@pytest.mark.parametrize('status, expected', [
+    ('200 OK', ''),
+    ('400 Bad Request', '127.0.0.1 - - [:datetime:] "GET / HTTP/1.1" 400 6\n'),
+], ids=['silent', 'not silent'])
+def test_SilentRequestHandler(capsys, status, expected):
     s = simple_server.WSGIServer(('', 0), SRH)
-    s.set_app(functools.partial(app, '200 OK'))
+    s.set_app(functools.partial(app, status))
     t = threading.Thread(target=functools.partial(s.serve_forever), daemon=True)
     t.start()
-    with request.urlopen('http://{}:{}/'.format(*s.server_address)) as r:
-        pass
-    s.shutdown()
-    t.join()
-    s.server_close()
-    out, err = capsys.readouterr()
-    assert err == ''
 
-def test_SilentRequestHandler_400(capsys):
-    s = simple_server.WSGIServer(('', 0), SRH)
-    s.set_app(functools.partial(app, '400 Bad Request'))
-    t = threading.Thread(target=functools.partial(s.serve_forever), daemon=True)
-    t.start()
     try:
         with request.urlopen('http://{}:{}/'.format(*s.server_address)) as r:
             pass
-    except error.HTTPError:
+    except error.HTTPError as e:
         pass
+
     s.shutdown()
     t.join()
     s.server_close()
+
     out, err = capsys.readouterr()
-    assert err == '127.0.0.1 - - [:datetime:] "GET / HTTP/1.1" 400 6\n'
+    assert err == expected
