2da62c4c8a7eac5601c5ffabe4f8e5100dded417
Sam Morris <sam@robots.org.uk>
2017-03-06 10:37:54 +0000

    Pull python module version from debian/changelog, in preference to using build no and git commit

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
+import os
 import subprocess
 
 from setuptools import setup
 
-try:
+# Prefer to use the version from the Debian package. Fall back to the version
+# from git. This way the .git directory does not have to be included in the
+# Debian source package.
+if os.path.exists('debian/changelog'):
+    for line in subprocess.check_output(['dpkg-parsechangelog']).decode().split('\n'):
+        tokens = line.split()
+        if tokens[0] == 'Version:':
+            version = tokens[1]
+            break
+else:
+    build_no = os.environ.get('BUILD_NUMBER', 'dev0')
     git_ref = subprocess.check_output(['git', 'rev-parse', '--verify', '--short', 'HEAD']).decode().rstrip()
-except Exception:
-    git_ref = '?'
+    version = '0.{}+g{}'.format(build_no, git_ref)
 
 setup(
     name = 'temper-exporter',
-    version = '0+g{}'.format(git_ref),
+    version = version,
     description = 'Prometheus exporter for PCSensor TEMPer sensor devices',
     url = 'https://github.com/yrro/temper-exporter',
     author = 'Sam Morris',
